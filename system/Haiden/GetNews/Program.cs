75adc74ca01875e088a518072dac7e68ef885a4c
willianzocolau <willianzocolau@outlook.com>
2018-08-11 23:13:26 -0300

    Initial project GetNews

---
+++ system/Haiden/GetNews/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GetNews
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
